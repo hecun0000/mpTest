441618e1b4897f27b36f2d47fc71579d4a086cb4
hecun <16395864338@qq.com>
2020-06-11 22:32:28 +0800

    init

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <swiper @change="swiperChange" indicator-dots="true" autoplay="true" interval="5000" duration="500" circular="true"
+                indicator-color="#D8D8D8" indicator-active-color="#E93C3E">
+            <swiper-item v-for="(item,index) in detailObject.picture_array" :key="index">
+                <image :src="item" :id="index" @load="imageLoad" class="slide-image" mode="aspectFit" style="width: 100%; height: 100%"></image>
+            </swiper-item>
+        </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    wx.setNavigationBarTitle({
+      title: '商品详情'
+    })
+  },
+  methods: {
+
+  }
+
+}
+</script>
+
+<style scoped>
+.save-button {
+  margin: 30rpx 10rpx;
+}
+</style>
