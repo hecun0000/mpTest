bf15f3d85e500e6be3c57225da572f5db7f1e3f7
hecun <16395864338@qq.com>
2020-06-19 00:47:16 +0800

    修改

--- src/pages/detail/share.vue
+++ src/pages/detail/share.vue
@@ -5,7 +5,7 @@
 
       </div>
       <div class="poster">
-        <canvas style="width: 681rpx; height:982rpx;" type="2d" id="ctx" canvas-id="ctx"></canvas>
+        <canvas style="width: 681rpx; height:890rpx;" type="2d" id="ctx" canvas-id="ctx"></canvas>
       </div>
       <div class="button-box">
         <van-button border type="primary" block @click="downloadImg">保存到本地</van-button>
@@ -17,18 +17,19 @@
 export default {
   data () {
     return {
+      width: 0,
+      height: 0,
       ctxWidth: null,
       ctxHeight: null,
       show: false,
       imgUrl: '',
       info: {
-
         title: '初秋新款德瑞毛茸高跟鞋标题标题标题标题标题可爱可爱可爱可爱可标题标题标题标题可爱可爱可标题标题标题标题可爱可爱可标题标题标题标题可爱可爱可爱 …',
         url: 'https://img11.360buyimg.com/n1/s450x450_jfs/t1/131421/9/1916/444202/5ee0d3fbE8d0281f3/11faf05514ecc347.jpg'
       }
     }
   },
-  mounted () {
+  onload () {
     const res = wx.getSystemInfoSync()
     this.windowWidth = res.windowWidth
     this.windowHeight = res.windowHeight
@@ -39,9 +40,10 @@ export default {
     },
     onClickShow () {
       this.show = true
-      this.init()
+      setTimeout(_ => {
+        this.init()
+      }, 500)
     },
-
     onClickHide () {
       this.show = false
     },
@@ -53,80 +55,73 @@ export default {
         .exec((res) => {
           console.log(res, 'res 2d')
           const canvas = res[0].node
-          // self.ctxWidth = res[0].width
-          // self.ctxHeight = res[0].height
           const dpr = wx.getSystemInfoSync().pixelRatio
           // 新接口需显示设置画布宽高；
-          canvas.width = res[0].width * dpr
-          canvas.height = res[0].height * dpr
+          const width = res[0].width * dpr
+          const height = res[0].height * dpr
+          self.width = width
+          self.height = width
+          canvas.width = width
+          canvas.height = height
           const ctx = canvas.getContext('2d')
-          ctx.scale(dpr, dpr)
-
-          console.log(ctx, 'ctx')
-          // ctx.setFillStyle('#66ccff')
-          ctx.fillStyle = '#66ccff'
-          ctx.fillRect(0, 0, 681, 982)
-          // ctx.draw()
-          // ctx.restore()
+          ctx.fillStyle = '#ffffff'
+          ctx.fillRect(0, 0, width, height)
+          // ctx.font = '100px Georgia'
+          // ctx.fillText('Hello World!', 10, 50)
           self.drawImg(ctx, canvas)
-          // self.drawTitle(ctx)
-          // self.canvasToImg()
+          self.drawTitle(ctx)
+          self.canvasToImg(canvas)
         })
-      var ctx = wx.createCanvasContext('ctx')
-      console.log(this.toPX(681))
-      console.log(ctx, 'ctx')
-      // ctx.save()
-      // ctx.beginPath()
     },
     drawImg (ctx, canvas) {
-      // const that = this
+      const that = this
       wx.downloadFile({
         url: this.info.url,
         success: function (res) {
           let img = canvas.createImage()
           img.onload = e => {
-            ctx.drawImage(img, 20, 20, 644, 644)
+            const padding = 40
+            ctx.drawImage(img, padding, padding, that.width - padding * 2, that.width - padding * 2)
           }
           img.src = res.tempFilePath
-          // ctx.save()
-          // ctx.beginPath()
-          // ctx.rect(that.toPX(20), that.toPX(20), that.toPX(644), that.toPX(644))
-          // ctx.clip()
-          // ctx.restore()
-          // ctx.draw(true)
         }
       })
     },
     drawTitle (ctx) {
-      const fontSize = 16
-      ctx.setFontSize(fontSize)
-      ctx.setFillStyle('#000000')
+      // ctx.font = '100px Verdana'
+      // ctx.fillText('禾寸', 80, 1210)
+      const fontSize = 20 * 2
+      // console.log(ctx, 'ctx')
+      ctx.font = fontSize + 'px Verdana'
+      ctx.fillStyle = '#000000'
       const str = this.info.title
-      const width = this.toPX(644 - 20)
+      const width = (this.width - 80)
       const size = Math.ceil(width / fontSize)
       var strArr = []
       for (var i = 0, l = str.length; i < l / size; i++) {
         var a = str.slice(size * i, size * (i + 1))
         strArr.push(a)
       }
-      let textTop = 710
+      console.log(strArr, 'strArr')
+      let textTop = 1140
       for (let j = 0; j < 2; j++) {
-        ctx.fillText(strArr[j], this.toPX(20), this.toPX(textTop))
-        textTop += 40
+        console.log(str, strArr[j], textTop, 'ress')
+        ctx.fillText(strArr[j], 40, textTop)
+        textTop += 60
       }
-      ctx.draw()
+      // ctx.draw()
     },
     drawUser (ctx) {
-
     },
-    canvasToImg () {
+    canvasToImg (canvas) {
+      console.log('dddddddddddddddddddd图片已保存到相册，赶紧晒 canvas')
       const self = this
       setTimeout(() => {
         // 将生成的canvas图片，转为真实图片
         wx.canvasToTempFilePath({
           x: 0,
           y: 0,
-          canvasId: 'ctx',
+          canvas,
           success: function (res) {
             console.log(res)
             self.imgUrl = res.tempFilePath
@@ -136,7 +131,6 @@ export default {
             //   showModal: true,
             //   showShareModal: false
             // })
-
             // wx.hideLoading()
           },
           fail: function (res) {
